f80747ab514d63192d719e3270aa8e724821c65c
Slluxx <Slluxx@users.noreply.github.com>
2025-02-12 19:26:40 +0100

    Update scriptloader.lua
    Added autofix option

--- scriptloader/scriptloader.lua
+++ scriptloader/scriptloader.lua
@@ -14,7 +14,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
 
-local config = ac.configValues({ url = "" })
+local config = ac.configValues({ url = "", autofix = false })
 local codeFunctions = nil
 
 if config.url == "" then return false end
@@ -24,10 +24,29 @@ web.get(config.url, {}, function (err, response)
         ac.debug("WEB ERROR:", err)
         ac.debug("Response Status: ", response.status)
         return
-     end
+    end
+
+    local codeString = response.body
+
+    if config.autofix == true then
+        if not string.match(codeString, "^%s*return%s*{.*[Uu]pdate.*}") or not string.match(codeString, "return%s*{.*[Dd]raw3D.*}") then
+            
+            local retfixCode = [[
+local rtable = {}
+if type(script.update) == "function" then table.insert(rtable, script.update) elseif type(update) == "function" then table.insert(rtable, update) end
+if type(script.draw3D) == "function" then table.insert(rtable, script.draw3D) elseif type(draw3D) == "function" then table.insert(rtable, draw3D) end
+return rtable
+            ]]
+
+            codeString = codeString .. "\n".. retfixCode
+            ac.debug("code", codeString)
+        end
+    end
+
+    
 
     try(function ()
-        local code = assert(loadstring(response.body))
+        local code = assert(loadstring(codeString))
         codeFunctions = code()
     end, function (err)
         ac.debug("Script execution error:", err)
