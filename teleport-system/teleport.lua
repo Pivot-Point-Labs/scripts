22e4dea83f6c9264536b950f3f6070cfe2b79a08
Slluxx <Slluxx@users.noreply.github.com>
2025-02-13 12:43:36 +0100

    Added a version number comment

--- teleport-system/teleport.lua
+++ teleport-system/teleport.lua
@@ -14,6 +14,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
 
+-- Version: 1.2
 
 ac.storageSetPath("slx_teleportconfig")
 
